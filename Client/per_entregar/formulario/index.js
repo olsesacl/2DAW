227c4f21e69f16c5193bc6c2dabcf0096b4aa9e0
Sergio Sanchis Climent <sergiosanchis@hotmail.com>
2015-11-11 16:37:53 +0100

    Exercicis client

--- Client/per_entregar/formulario/index.js
+++ Client/per_entregar/formulario/index.js
@@ -33,6 +33,8 @@ function validar(){
 
     //validar text
     errors +=validar_text();
+    errors +=validar_radio();
+    errors +=validar_time();
 
    this.disabled=false;
     this.value='Submit';
@@ -54,12 +56,67 @@ function validar_text(){
        if(textos[i].value.length == 0){
            errors++;
 
-           var iDiv = document.createElement('div');
-           iDiv.name = 'validate';
-           iDiv.className = 'validate_ko';
-           iDiv.innerHTML = "El campo no puede estar vacio";
-           textos[i].parentNode.appendChild(iDiv);
+          mostrar_error(textos[i], "El campo no puede estar vacio");
        }
     }
     return errors;
+}
+function validar_radio(){
+    opciones = document.getElementsByName("element_13");
+
+    var seleccionado = 0;
+    for(var i=0; i<opciones.length; i++) {
+        if(opciones[i].checked) {
+            seleccionado = 1;
+            break;
+        }
+    }
+    if(seleccionado == 0){
+        element_error = document.getElementById('li_13');
+        mostrar_error(element_error.lastChild, "Se ha de seleccionar una opcion");
+    }
+
+    return seleccionado;
+}
+
+function validar_time(){
+    var horas = document.getElementById('element_4_1').value;
+    var minutos = document.getElementById('element_4_2').value;
+    var segundos = document.getElementById('element_4_3').value;
+
+    var error = 0;
+
+    var message = '';
+
+    if(horas > 12 || horas < 1 || isNaN(horas)){
+        error = 1;
+        message += 'La hora ha de ser un numero entre 1 y 12<br>'
+    }
+    if(minutos > 59 || minutos < 0 || isNaN(minutos)){
+        error = 1;
+        message += 'Los minutos han de ser un numero entre 0 y 59<br>'
+    }
+    if(segundos > 59 || segundos < 0 || isNaN(segundos)){
+        error = 1;
+        message += 'Los segundos han de ser un numero entre 0 y 59<br>'
+    }
+
+    if(error){
+        element_error = document.getElementById('li_4');
+        mostrar_error(element_error.lastChild, message);
+    }
+
+    return error;
+
+
+
+}
+
+
+function mostrar_error(elemento ,text){
+
+    var iDiv = document.createElement('div');
+    iDiv.className = 'validate_ko';
+    iDiv.innerHTML = text;
+    elemento.parentNode.appendChild(iDiv);
 }
