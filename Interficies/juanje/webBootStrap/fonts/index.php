1b8a6b29abc407051e6b777dad6c4f566161d7b9
Sergio Sanchis Climent <sergiosanchis@hotmail.com>
2016-03-02 22:58:16 +0100

    bootstrap juanje

---
+++ Interficies/juanje/webBootStrap/fonts/index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML PUBLIC “-//W3C//DTD HTML 4.01 Strict//EN” 
+“http://www.w3.org/TR/html4/strict.dtd”>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<!--<base href="http://www.generic.com/">-->
+		
+		<link href="../reset.css" rel="stylesheet" type="text/css">
+		<link href="../estil.css" rel="stylesheet" type="text/css">
+		<title>Trendy Wagon - Tu Tienda Online</title>
+
+		
+	
+
+	</head>
+
+	<body>
+		<div id="contenedor">
+			
+			<div id="cabecera"></div>
+			
+			
+			<div id="menu_horizontal">
+				<div id="menu">
+					<ul class="menu">
+						
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Ropa</a></li>
+						<li><a href="#">Complementos</a></li>
+						<li><a href="#">Bicis</a></li>
+						<li><a href="#">Skate</a></li>
+						<li><a href="#">Surf</a></li>
+						
+					</ul>
+				</div>
+				<div id="redes">
+					<ul class="redes">
+					    <li><a href="#"><img src="../images/twitter.png"></a></li>
+						<li><a href="#"><img src="../images/facebook.png"></a></li>
+						<li><a href="#"><img src="../images/rss.png"></a></li>
+
+				    </ul>
+				</div>
+               <div id="login">
+
+               		<ul class="login">
+					
+					    <li><a href="#"><img src="../images/carrito.png"></a></li>
+						<li><a href="#">LOGIN</a></li>
+						
+
+			         </ul>
+			    </div>
+				
+
+			</div>
+
+
+
+
+			<div id="contenido">
+                 
+			  <div id="producto">
+
+                 <div id="img_prod"><a href="" title="Bicicleta surfera + Portatablas"><img src="../images/bici.jpg"></a></div>
+                 <div id="desc_prod"><p>Bicicleta surfera + Portatablas</p></div>
+                 <div id="carrito"><a href="" title="Añadir al carrito"><img src="../images/carrito_prod.png"></a></div>
+                 <div id="preu_prod"><p>499€</p></div>
+                
+
+			  </div>
+			   <div id="producto">
+
+                 <div id="img_prod"><a href="" title="Tabla de surf Fish"><img src="../images/tabla.jpg"></a></div>
+                 <div id="desc_prod"><p>Tabla de surf Fish</p></div>
+                 <div id="carrito"><a href="" title="Añadir al carrito"><img src="../images/carrito_prod.png"></a></div>
+                 <div id="preu_prod"><p>399€</p></div>
+                
+
+			  </div>
+			   <div id="producto">
+
+                 <div id="img_prod"><a href="" title="Gafas de Sol"><img src="../images/gafas.jpg"></a></div>
+                 <div id="desc_prod"><p>Gafas de sol</p></div>
+                 <div id="carrito"><a href="" title="Añadir al carrito"><img src="../images/carrito_prod.png"></a></div>
+                 <div id="preu_prod"><p>79€</p></div>
+                
+
+			  </div>
+			   <div id="producto">
+
+                 <div id="img_prod"><a href="" title="Sudadera"><img src="../images/sudadera.jpg"></a></div>
+                 <div id="desc_prod"><p>Sudadera</p></div>
+                 <div id="carrito"><a href="" title="Añadir al carrito"><img src="../images/carrito_prod.png"></a></div>
+                 <div id="preu_prod"><p>45€</p></div>
+                
+
+			  </div>
+			   <div id="producto">
+
+                 <div id="img_prod"><a href="" title="Zapatillas"><img src="../images/zapatillas.jpg"></a></div>
+                 <div id="desc_prod"><p>Zapatillas</p></div>
+                 <div id="carrito"><a href="" title="Añadir al carrito"><img src="../images/carrito_prod.png"></a></div>
+                 <div id="preu_prod"><p>49€</p></div>
+                
+
+			  </div>
+			   <div id="producto">
+
+                 <div id="img_prod"><a href="" title="Skate Longboard"><img src="../images/skate.jpg"></a></div>
+                 <div id="desc_prod"><p>Skate Longboard</p></div>
+                 <div id="carrito"><a href="" title="Añadir al carrito"><img src="../images/carrito_prod.png"></a></div>
+                 <div id="preu_prod"><p>149€</p></div>
+                
+
+			  </div>
+
+
+			 
+			   
+			</div>
+
+		   <div id="cierre"></div>
+
+           
+
+                
+        </div>
+
+		 <div id="pie">
+
+		                 
+		    <div id="pie_in">
+
+			
+
+			        <ul>
+						<li><a href="#"><img src="../images/logo_peu.png"></a></li>
+						<li><a href="../nosotros.html">Sobre&nbsp;&nbsp;Nosotros</a></li>
+						<li><a href="#">Contacto</a></li>
+						<li><a href="#">FAQs</a></li>
+											
+												
+					</ul>
+
+                      <ul class="pagos">
+											
+						<li><a href="#"><img src="../images/visa.png"></a></li>
+						<li><a href="#"><img src="../images/paypal.png"></a></li>
+					
+											
+												
+					</ul>
+
+
+		    </div>
+		                
+
+
+		</div>
+
+		
+	</body>
+</html>
